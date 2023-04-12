8b68d1f30d794b5eb022aa8711525e725b0a8fda
ri-ca4 <riley.de12@gmail.com>
2023-04-11 23:43:33 -0400

    add axios request

--- server/index.js
+++ server/index.js
@@ -1,8 +1,28 @@
 const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const axios = require('axios');
 
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(cors());
+app.use(express.json());
+
+app.get('/weather', (req, res) => {
+    const options = {
+        method:'GET',
+        url: `https://api.openweathermap.org/data/2.5/weather?zip=32066,US&appid=${process.env.WEATHER_API_KEY}`,
+      }
+  
+      axios.request(options).then((response)=>{
+        console.log(response)
+        res.json(response.data)
+      }).catch((error)=>{
+        console.log(error)
+      })
+
+});
 
 app.listen(port, ()=>{
     console.log(`listening on ${port}`)
